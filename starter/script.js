df31be61cf87bea0a2919cffdb849a37c79ed2b4
Johannes Andersen <Johannes545@gmail.com>
2022-08-16 10:23:53 +0200

    refactored my code, so its cleaner and i dont repeat myself

--- starter/script.js
+++ starter/script.js
@@ -2,48 +2,57 @@
 
 let randomNumber = Math.trunc(Math.random() * 20) + 1
 
+function redScreen() {
+    document.querySelector("body").style.backgroundColor = "red"
+};
+
 document.querySelector(".number").textContent = randomNumber
 
-let message = document.querySelector(".message")
+function message(text) {
+    document.querySelector(".message").textContent = text
+}
+
+function displayScore(score) {
+    document.querySelector(".score").textContent = score
+}
 
 let score = 20
-let displayScore = document.querySelector(".score")
 // message.textContent = "🎉 Congrats it the right number 🎉"
 
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value)
     console.log(typeof guess, guess)
     if(guess<=0) {
-        message.textContent ="Please guess in the range of 1-20 "
+        message("Please guess in the range of 1-20")
     } else if(guess > randomNumber) {
         if(score > 1) {
-        message.textContent = "⬇️ Too High";
+        message("⬇️ Too High")
         score--;
-        displayScore.textContent = score
+        displayScore(score)
         }else {
-            message.textContent = "You lost, Try again"
-            displayScore.textContent = 0
-            document.querySelector("body").style.backgroundColor = "red"
+            message("You lost, Try again")
+            displayScore(0)
+            redScreen();
         }
     } else if(guess < randomNumber) {
         if(score > 1) {
-        message.textContent = "⬆️ Too Low";
+        message("⬆️ Too Low")
         score--
-        displayScore.textContent = score
+        displayScore(score)
         } else {
-            message.textContent = "You lost, Try again"
-            displayScore.textContent = 0
-            document.querySelector("body").style.backgroundColor = "red"
+            message("You lost, Try again")
+            displayScore(0)
+            redScreen()
         }
     } else if (guess === randomNumber) {
         if(score >1){
-        message.textContent = "🎉Congrats, you found the number🎉"
+        message("🎉Congrats you found the number 🎉")
         document.querySelector("body").style.backgroundColor = "green"
         document.querySelector(".highscore").textContent = score
         }else{
-            message.textContent = "You lost, Try again"
-            displayScore.textContent = 0
-            document.querySelector("body").style.backgroundColor = "red"
+            message("You lost, Try again")
+            displayScore(0)
+            redScreen()
         }
     }
 })
@@ -51,8 +60,8 @@ document.querySelector(".check").addEventListener("click", function() {
 document.querySelector(".again").addEventListener("click", function(){
     document.querySelector("body").style.backgroundColor = "#222"
     score = 20
-    displayScore.textContent = score
-    message.textContent = "Guess My Number!"
+    displayScore(score)
+    message("Guess My Number!")
     randomNumber = Math.trunc(Math.random() * 20) + 1
     document.querySelector(".number").textContent = randomNumber
 })
